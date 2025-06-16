29d1c58576bf7242062ba82bec42ab8e59096e38
MaximoFC <maximofcallejas@gmail.com>
2025-06-16 17:18:25 -0300

    user authentication in development

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import authRoutes from './routes/auth.routes.js'
 
 dotenv.config();
 connectDB();
@@ -10,5 +11,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api', authRoutes);
 
 export default app;
---
+++ src/models/employee.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const employeeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'employee'],
+        default: 'employee'
+    }
+});
+
+export default mongoose.model('Employee', employeeSchema);
