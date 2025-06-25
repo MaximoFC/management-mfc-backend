6d8707b649690cb42a2d5b8efa989cd7bb02d7b4
MaximoFC <maximofcallejas@gmail.com>
2025-06-25 02:12:09 -0300

    get profile added in auth controller, profile route added in auth routes and next() added in auth middleware

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { login } from '../controllers/auth.controller.js'
+import { getProfile, login } from '../controllers/auth.controller.js';
+import { tokenVerify } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 router.post('/login', login);
+router.get('/profile', tokenVerify, getProfile);
 
 export default router;
