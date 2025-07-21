8dc3702c1e815a8b9b24d449d4f18aa0511f0c9f
MaximoFC <maximofcallejas@gmail.com>
2025-07-21 14:27:22 -0300

    implement budget creation with real-time ARS conversion using Bluelytics API

---
+++ src/utils/getDollarRate.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const getDollarBlueRate = async () => {
+    try {
+        const response = await axios.get('https://api.bluelytics.com.ar/v2/latest');
+        return response.data.blue.value_sell;
+    } catch (error) {
+        console.error('Error getting dollar rate: ', error.message);
+        return 1000;
+    }
+};
