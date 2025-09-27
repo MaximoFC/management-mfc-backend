62dd1a144343b198887a73ac952ffbf2b2299c38
MaximoFC <maximofcallejas@gmail.com>
2025-09-27 14:13:36 -0300

    error log in catch and timeout added

--- src/utils/getDollarRate.js
+++ src/utils/getDollarRate.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getDollarBlueRate = async () => {
     try {
-        const response = await axios.get('https://api.bluelytics.com.ar/v2/latest');
+        const response = await axios.get('https://api.bluelytics.com.ar/v2/latest', { timeout: 5000 });
         return response.data.blue.value_sell;
     } catch (error) {
         console.error('Error getting dollar rate: ', error.message);
