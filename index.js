8992412eda81d2e47eaa1d8ceda23bd875898efc
MaximoFC <maximofcallejas@gmail.com>
2025-09-27 13:57:13 -0300

    error handling added

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import './src/jobs/warranties.job.js';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, (error) => {
+    if (error) {
+        console.error("Error starting server: ", error);
+        process.exit(1);
+    }
     console.log(`Server on port ${PORT}`);
 });
