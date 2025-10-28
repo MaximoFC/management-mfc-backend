bc0f570b2097830bb482ae432e30226f4e064a1e
MaximoFC <maximofcallejas@gmail.com>
2025-10-28 09:42:33 -0300

    dollar rate cached

--- src/utils/getDollarRate.js
+++ src/utils/getDollarRate.js
@@ -1,12 +1,24 @@
 import axios from "axios";
 
+let cachedRate = null;
+let lastFetchTime = 0;
+
 const getDollarBlueRate = async () => {
+    const now = Date.now();
+    const fifteenMinutes = 15 * 60 * 1000;
+    
+    if (cachedRate && (now - lastFetchTime < fifteenMinutes)) {
+        return cachedRate;
+    }
+
     try {
         const response = await axios.get('https://api.bluelytics.com.ar/v2/latest', { timeout: 5000 });
-        return response.data.blue.value_sell;
+        cachedRate = response.data.blue.value_sell;
+        lastFetchTime = now;
+        return cachedRate;
     } catch (error) {
         console.error('Error getting dollar rate: ', error.message);
-        return 1000;
+        return cachedRate || 1000;
     }
 };
 
