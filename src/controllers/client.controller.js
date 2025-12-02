96c70aa1786d90522eec110caa0d24bcfa87d987
NachoConort <nachoconort@gmail.com>
2025-12-02 13:24:57 -0300

    client search fixed

--- src/controllers/client.controller.js
+++ src/controllers/client.controller.js
@@ -3,82 +3,83 @@ import Bike from "../models/bike.model.js";
 import Budget from "../models/budget.model.js";
 
 export const createClient = async (req, res) => {
-    if (!req.body.name || !req.body.surname || !req.body.mobileNum) {
-        return res.status(400).json({ error: "Missing required fields" });
-    }
+  if (!req.body.name || !req.body.surname || !req.body.mobileNum) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
 
-    try {
-        const client = new Client(req.body);
-        await client.save();
-        res.status(201).json(client);
-    } catch (error) {
-        if (error.code === 11000) { // llave duplicada (mobileNum con unique)
-            return res.status(409).json({ error: "Mobile number already exists" });
-        }
-        res.status(500).json({ error: 'Error creating client' });
+  try {
+    const client = new Client(req.body);
+    await client.save();
+    res.status(201).json(client);
+  } catch (error) {
+    if (error.code === 11000) {
+      // llave duplicada (mobileNum con unique)
+      return res.status(409).json({ error: "Mobile number already exists" });
     }
+    res.status(500).json({ error: "Error creating client" });
+  }
 };
 
 export const getClients = async (req, res) => {
-    try {
-        //lo agregue para que se banque la busqueda en el navbar
-        const { q } = req.query;
-        let filter = {};
-        if (q) {
-            filter = {
-                $or: [
-                    { name: { $regex: q, $options: 'i' } },
-                    { surname: { $regex: q, $options: 'i' } },
-                    { mobileNum: { $regex: q, $options: 'i' } }
-                ]
-            };
-        }
-        const clients = await Client.find(filter).sort({ createdAt: -1 }).lean();
-        res.json(clients);
-    } catch (error) {
-        res.status(500).json({ error: 'Error getting clients' });
+  try {
+    //lo agregue para que se banque la busqueda en el navbar
+    const { q } = req.query;
+    let filter = {};
+    if (q) {
+      filter = {
+        $or: [
+          { name: { $regex: "^" + q, $options: "i" } },
+          { surname: { $regex: "^" + q, $options: "i" } },
+          { mobileNum: { $regex: "^" + q, $options: "i" } },
+        ],
+      };
     }
+    const clients = await Client.find(filter).sort({ createdAt: -1 }).lean();
+    res.json(clients);
+  } catch (error) {
+    res.status(500).json({ error: "Error getting clients" });
+  }
 };
 
 export const getClientsById = async (req, res) => {
-    try {
-        const client = await Client.findById(req.params.id);
+  try {
+    const client = await Client.findById(req.params.id);
 
-        if (!client) return res.status(404).json({ error: "Client not found" });
+    if (!client) return res.status(404).json({ error: "Client not found" });
 
-        const bikes = await Bike.find({ current_owner_id: client._id });
+    const bikes = await Bike.find({ current_owner_id: client._id });
 
-        res.json({ client, bikes });
-    } catch (error) {
-        res.status(500).json({ error: 'Error getting client' });
-    }
+    res.json({ client, bikes });
+  } catch (error) {
+    res.status(500).json({ error: "Error getting client" });
+  }
 };
 
 export const updateClient = async (req, res) => {
-    try {
-        const client = await Client.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true, runValidators: true }
-        );
+  try {
+    const client = await Client.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-        if (!client) return res.status(404).json({ error: "Client not found" });
-        res.json(client);
-    } catch (error) {
-        if (error.code === 11000) { // llave duplicada (mobileNum con unique)
-            return res.status(409).json({ error: "Mobile number already exists" });
-        }
-        res.status(500).json({ error: 'Error updating client' });
+    if (!client) return res.status(404).json({ error: "Client not found" });
+    res.json(client);
+  } catch (error) {
+    if (error.code === 11000) {
+      // llave duplicada (mobileNum con unique)
+      return res.status(409).json({ error: "Mobile number already exists" });
     }
+    res.status(500).json({ error: "Error updating client" });
+  }
 };
 
 export const deleteClient = async (req, res) => {
-    try {
-        const client = await Client.findByIdAndDelete(req.params.id);
-        if (!client) return res.status(404).json({ error: "Client not found" });
-        await Bike.deleteMany({ current_owner_id: req.params.id });
-        res.json({ message: 'Client and bikes deleted' });
-    } catch (error) {
-        res.status(500).json({ error: 'Error deleting client' });
-    }
+  try {
+    const client = await Client.findByIdAndDelete(req.params.id);
+    if (!client) return res.status(404).json({ error: "Client not found" });
+    await Bike.deleteMany({ current_owner_id: req.params.id });
+    res.json({ message: "Client and bikes deleted" });
+  } catch (error) {
+    res.status(500).json({ error: "Error deleting client" });
+  }
 };
