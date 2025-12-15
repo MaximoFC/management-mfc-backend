b9979e84408971bf34d28796b860c65d1b753d1f
NachoConort <nachoconort@gmail.com>
2025-12-15 12:58:29 -0300

    Refactor clients and bikes

--- src/controllers/bootstrap.controller.js
+++ src/controllers/bootstrap.controller.js
@@ -1,17 +1,23 @@
 import Service from "../models/service.model.js";
 import BikePart from "../models/bikepart.model.js";
+import Client from "../models/client.model.js";
+import Bike from "../models/bike.model.js";
 
 export const getBootstrapData = async (req, res) => {
-    try {
-        const services = await Service.find().sort({ name: 1 }).lean();
-        const bikeparts = await BikePart.find().sort({ brand: 1 }).lean();
+  try {
+    const services = await Service.find().sort({ name: 1 }).lean();
+    const bikeparts = await BikePart.find().sort({ brand: 1 }).lean();
+    const clients = await Client.find().sort({ name: 1, surname: 1 }).lean();
+    const bikes = await Bike.find().sort({ brand: 1, model: 1 }).lean();
 
-        res.json({
-            services,
-            bikeparts
-        });
-    } catch (err) {
-        console.error("Error en bootstrap: ", err.message);
-        res.status(500).json({ error: "Error obteniendo datos iniciales" });
-    }
+    res.json({
+      services,
+      bikeparts,
+      clients,
+      bikes,
+    });
+  } catch (err) {
+    console.error("Error en bootstrap: ", err.message);
+    res.status(500).json({ error: "Error obteniendo datos iniciales" });
+  }
 };
